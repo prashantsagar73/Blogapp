e3ddafbbd9a6403528c1ee732ae857c5d99dd6ff
Prashant sagar <prashant.sagar73@gmail.com>
2021-05-09 12:34:33 +0530

    models and database of blog

--- blog/blogp/models.py
+++ blog/blogp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class post(models.Model):
+    title = models.TextField(max_length=150)
+    content = models.CharField(max_length=5000)
+    date_time= models.DateField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/blogp/views.py
+++ blog/blogp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from .models import post
 # Create your views here.
-def home (request):
-    return render(request,"home.html")
+posts =[
+    {
+        'author': "prashant sagar",
+        'title': 'this is jus testing',
+        'content': 'hello how are everything going on?',
+    },
+    {
+        'author': "prashant sagar",
+        'title': 'this is jus testing',
+        'content': 'hello how are everything going on?',
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts': post.objects.all()
+    }
+    return render(request,"home.html",context)
+
+def about(request):
+    return render(request,"about.html")
